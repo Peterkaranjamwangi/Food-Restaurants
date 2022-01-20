574ec144731e62ed3fe236392f7902e2817abee5
Peter Mwangi <essayhelpwritersofficial@gmail.com>
2022-01-20 09:16:15 +0300

    Added Routes to Collection

--- src/components/collections.vue
+++ src/components/collections.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <v-container py-15 pd>
+    <v-container py-15>
       <h1>Collections</h1>
       <p>
         Explore curated lists of top restaurants, cafes, pubs, and bars in New
@@ -8,8 +8,8 @@
       </p>
       <br />
       <v-row dense>
-        <v-col v-for="card in collections" :key="card.title" cols="12" md="4">
-          <v-card>
+        <v-col cols="12" md="4" v-for="card in collections" :key="card.title">
+          <v-card router :to="card.route">
             <v-img
               :src="card.src"
               class="white--text align-end"
@@ -17,7 +17,7 @@
               height="200px"
             >
               <v-card-title v-text="card.title"></v-card-title>
-              <div class="d-flex">
+              <div class="d-flex" router :to="card.route">
                 <p class="text-sm white--text mb-0">30 Places</p>
                 <i
                   aria-hidden="true"
@@ -41,19 +41,19 @@ export default {
         title: "Pre-fab homes",
         src: "https://images.unsplash.com/photo-1504754524776-8f4f37790ca0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8Zm9vZHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
         flex: 12,
-        route: "/files",
+        route: "/about",
       },
       {
         title: "Favorite road trips",
         src: "https://images.unsplash.com/photo-1473093295043-cdd812d0e601?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTZ8fGZvb2R8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
         flex: 12,
-        route: "/files",
+        route: "/about",
       },
       {
         title: "Best airlines",
         src: "https://images.unsplash.com/photo-1529042410759-befb1204b468?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTd8fGZvb2R8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
         flex: 12,
-        route: "/files",
+        route: "/about",
       },
     ],
   }),
